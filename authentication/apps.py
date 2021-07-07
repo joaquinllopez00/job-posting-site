3639e450e5a086c2729033d0f5bbcc054533c845
jenmcdonnell <jennymcdonn@gmail.com>
2021-07-07 15:09:45 -0400

    updated model, running into issues with migrations

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-AuthenticationConfig(AppConfig):
+
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
--- user/models.py
+++ user/models.py
@@ -2,14 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class BaseUser(AbstractUser):
-    full_name = models.CharField(max_length=150)
+class User(AbstractUser):
+    classification = (('employer', 'Employer'), ('employee', 'Employee'))
+    name = models.CharField(max_length=150)
     bio = models.TextField(null=True, blank=True)
     signed_up = models.DateTimeField(auto_now=True)
-    email = models.EmailField(unique=True)
-    is_employer = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, blank=False)
+    role = models.CharField(choices=classification,
+                            default=False, max_length=10)
     # Uncomment & fill in model arg when ready
     # watch_list = models.ManyToManyField(JOB_POST, null=True, blank=True)
 
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     def __str__(self):
-        return self.email
+        return self.name + self.email
