d5eae18f508850f49e77e90a7ebe2f7cfae8be99
Crystal-Mackey <moonrunner575@gmail.com>
2021-07-07 15:22:16 -0400

    adjust issues with migration and user model

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import BaseUser
+from .models import User
 
 
-admin.site.register(BaseUser, UserAdmin)
+admin.site.register(User, UserAdmin)
