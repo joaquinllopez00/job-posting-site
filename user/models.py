af39e0c4bae03591c4535e5ef49f09de8dfbd9e8
Crystal-Mackey <moonrunner575@gmail.com>
2021-07-09 14:30:01 -0400

    made templates more comformed for now to allow for less eye strain as working on them

--- user/models.py
+++ user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from job.models import Listing
+
 class User(AbstractUser):
     classification = (('employer', 'Employer'), ('employee', 'Employee'))
     name = models.CharField(max_length=150)
